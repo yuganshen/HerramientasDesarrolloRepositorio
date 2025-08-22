25c9b90a387726acbe85376abe424ece927b80f6
Luis Fernando <luchohuilcahusca@gmail.com>
2025-08-21 22:50:43 -0500

    Se genero constructores a la case empleado y la clase personaa

--- Desarrollo/src/main/java/com/Herramientas/Desarrollo/Entidades/Empleado.java
+++ Desarrollo/src/main/java/com/Herramientas/Desarrollo/Entidades/Empleado.java
@@ -4,5 +4,12 @@ public class Empleado {
 	private String nombre;
 	private String apellido;
 	private String email;
+	
+	public Empleado(String nombre, String apellido, String email) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+	}
 
+	
 }
--- Desarrollo/src/main/java/com/Herramientas/Desarrollo/Entidades/Persona.java
+++ Desarrollo/src/main/java/com/Herramientas/Desarrollo/Entidades/Persona.java
@@ -1,15 +1,19 @@
 package com.Herramientas.Desarrollo.Entidades;
 
 public class Persona {
-private String ID;
+	private String ID;
+	private String nombre;
+	private String Juego;
+	private String apellido;
+	
+	public Persona(String iD, String nombre, String juego, String apellido) {
+		ID = iD;
+		this.nombre = nombre;
+		Juego = juego;
+		this.apellido = apellido;
+	}
 
-private String nombre;
-
-
-private String Juego;
-
-private String apellido;
-
+	
 
 
 }
