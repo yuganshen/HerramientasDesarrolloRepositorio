abd82adb387a7cf7b44c4af21716b8c13585f9ec
yuganshen <120036429+yuganshen@users.noreply.github.com>
2025-08-31 22:32:04 -0500

    Update ControllerSubOpciones.java

--- desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllerSubOpciones.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllerSubOpciones.java
@@ -7,4 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping ("/laptos")
 public class ControllerSubOpciones {
 
+  @GetMapping("/marca")
+  public String marca(){
+    return "marcas";
+  }
+
 }
