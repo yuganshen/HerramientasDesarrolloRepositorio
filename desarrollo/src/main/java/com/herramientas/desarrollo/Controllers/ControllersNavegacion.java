2e45a84e0dac702e32a8954c4c730e5d05ec8dee
ShadowIX-9 <ramosenderplayyt@gmail.com>
2025-09-06 11:27:41 -0500

    Agrege un nuevo mapping navbar

--- desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllersNavegacion.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllersNavegacion.java
@@ -16,7 +16,11 @@ public class ControllersNavegacion {
 		return"Login";
 	}
 	
-
+	@GetMapping("/navbar")
+	public String navbar() {
+		return"NavBar";
+	}
+	
 	@GetMapping("/registrar")
 	public String registrarNuevoUsuario(Model model){
 		model.addAttribute("registroUsuarioDto", new RegistroUsuarioDto());
