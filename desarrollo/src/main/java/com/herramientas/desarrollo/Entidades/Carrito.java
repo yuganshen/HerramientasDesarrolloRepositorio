d871ce452a17385ddf0d3c2e7dde5f276dc39b47
Oscar <oscaresquivel604@gmail.com>
2025-09-13 23:50:07 -0500

    atributos de carrito

--- desarrollo/src/main/java/com/herramientas/desarrollo/Entidades/Carrito.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Entidades/Carrito.java
@@ -2,4 +2,9 @@ package com.herramientas.desarrollo.Entidades;
 
 public class Carrito {
 
+	public String idUsuario;
+	public String idProducto;
+	public String idCarrito;
+	public String fechaCreacion;
+	
 }
