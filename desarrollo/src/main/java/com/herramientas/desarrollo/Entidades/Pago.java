a304228ad983c829b064535e810b592c96e1088a
Luis Fernando <luchohuilcahusca@gmail.com>
2025-09-14 21:34:25 -0500

    se agregaron los get y set de la clase pago

--- desarrollo/src/main/java/com/herramientas/desarrollo/Entidades/Pago.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Entidades/Pago.java
@@ -10,4 +10,61 @@ public class Pago {
     private String referencia;           // Referencia de la transacción (opcional)
     private String descripcion;          // Descripción o concepto del pago
     private String idUsuario;            // ID del usuario que realizó el pago
+	public String getIdPago() {
+		return idPago;
+	}
+	public void setIdPago(String idPago) {
+		this.idPago = idPago;
+	}
+	public double getMonto() {
+		return monto;
+	}
+	public void setMonto(double monto) {
+		this.monto = monto;
+	}
+	public String getMoneda() {
+		return moneda;
+	}
+	public void setMoneda(String moneda) {
+		this.moneda = moneda;
+	}
+	public String getFechaPago() {
+		return fechaPago;
+	}
+	public void setFechaPago(String fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+	public String getMetodoPago() {
+		return metodoPago;
+	}
+	public void setMetodoPago(String metodoPago) {
+		this.metodoPago = metodoPago;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getReferencia() {
+		return referencia;
+	}
+	public void setReferencia(String referencia) {
+		this.referencia = referencia;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+    
+    
+    
 }
