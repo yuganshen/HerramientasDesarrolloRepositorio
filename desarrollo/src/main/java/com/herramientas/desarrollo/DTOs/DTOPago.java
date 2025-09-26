47aef138e42849c50bb0c59b1a7b9d609fa3934b
Luis Fernando <luchohuilcahusca@gmail.com>
2025-09-25 21:37:51 -0500

    se crearon los atributos DTOPago

--- desarrollo/src/main/java/com/herramientas/desarrollo/DTOs/DTOPago.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/DTOs/DTOPago.java
@@ -1,5 +1,16 @@
 package com.herramientas.desarrollo.DTOs;
 
 public class DTOPago {
+	
+	private Long idPago;                
+    private double monto;                 
+    private String moneda;                
+    private	String fechaPago;     
+    private String metodoPago;           
+    private String estado;               
+    private String referencia;           
+    private String descripcion;          
+    private String idUsuario;            
 
+	
 }
