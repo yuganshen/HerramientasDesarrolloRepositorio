9b360d3962ae0a8b0fff1195bb7fb4ea32ccb12f
Pinpo-llo <oscaresquivel604@gmail.com>
2025-09-26 23:40:25 -0500

    Update ControllersNavegacion.java

--- desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllersNavegacion.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllersNavegacion.java
@@ -46,6 +46,10 @@ public class ControllersNavegacion {
 	public String tecladiños() {
 		return"teclado";
 	}
+	@GetMapping("/producto")
+	public String productos() {
+		return"productos";
+	}
 	
 	
 
