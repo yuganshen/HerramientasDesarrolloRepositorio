db138967f79f54af0ad4af887ad331eb1f9712d0
Luis Fernando <luchohuilcahusca@gmail.com>
2025-10-05 19:39:54 -0500

    se crearon atributos de DTOPedido

--- desarrollo/src/main/java/com/herramientas/desarrollo/DTOs/DTOPedido.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/DTOs/DTOPedido.java
@@ -1,5 +1,11 @@
 package com.herramientas.desarrollo.DTOs;
 
 public class DTOPedido {
+	public int idPedido;
+	public int idUsuario;
+	public int idProducto;
+	public String estado;
+	public double total;
+	public String fecha;
 
 }
