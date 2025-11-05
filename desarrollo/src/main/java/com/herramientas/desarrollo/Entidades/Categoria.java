9ff36eb9cb66ea4558da306344191adaad0aa8ff
Luis Fernando <luchohuilcahusca@gmail.com>
2025-11-04 21:52:40 -0500

    get set de categorias

--- desarrollo/src/main/java/com/herramientas/desarrollo/Entidades/Categoria.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Entidades/Categoria.java
@@ -35,5 +35,21 @@ public class Categoria {
 		this.estado = estado;
 	}
 
+	public Long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(Long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
     
 }
