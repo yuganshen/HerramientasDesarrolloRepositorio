080ef0c570b9b2cddb29faa1144c1c9059766727
yuganshen <120036429+yuganshen@users.noreply.github.com>
2025-11-14 21:34:55 -0500

    Interfaces repo

--- desarrollo/src/main/java/com/herramientas/desarrollo/Entidades/Pago.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Entidades/Pago.java
@@ -3,8 +3,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "pago")
 public class Pago {
 	
 	@Id
---
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Repositorios/CategoriaRepositorio.java
@@ -0,0 +1,10 @@
+package com.herramientas.desarrollo.Repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.herramientas.desarrollo.Entidades.Categoria;
+
+
+public interface CategoriaRepositorio extends JpaRepository <Categoria, Long>{
+
+}
