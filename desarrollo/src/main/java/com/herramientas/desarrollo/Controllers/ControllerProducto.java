16fcfd08a1879c8f966bd2ddeca3242a5181daf2
Luis Fernando <luchohuilcahusca@gmail.com>
2025-11-30 22:52:06 -0500

    Funcionalidd de catalogo con el back

---
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllerProducto.java
@@ -0,0 +1,31 @@
+package com.herramientas.desarrollo.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.herramientas.desarrollo.Entidades.Producto;
+import com.herramientas.desarrollo.Repositorios.ProductoRepositorio;
+
+
+@RestController
+@RequestMapping("/apiproducto")
+@CrossOrigin(origins = "http://localhost:4200") // permite Angular local
+public class ControllerProducto {
+	
+	
+	@Autowired
+	private ProductoRepositorio productoRepositorio;
+	
+	@GetMapping("/obtenerProductos")
+	public ResponseEntity<List<Producto>> obtenerProductos() {
+	    return ResponseEntity.ok(productoRepositorio.findAll());
+	}
+	
+	
+}
