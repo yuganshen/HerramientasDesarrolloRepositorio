84225ec42dde6e34b149d3e6cb7c992b8d205bb1
Luis Fernando <luchohuilcahusca@gmail.com>
2025-12-04 14:42:53 -0500

    metodo get para filtro

--- desarrollo/src/main/java/com/herramientas/desarrollo/Repositorios/ProductoRepositorio.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Repositorios/ProductoRepositorio.java
@@ -2,8 +2,11 @@ package com.herramientas.desarrollo.Repositorios;
 
 import com.herramientas.desarrollo.Entidades.Producto;
 
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
+	
+	List<Producto> findByTipoProducto(String tipo);
 }
