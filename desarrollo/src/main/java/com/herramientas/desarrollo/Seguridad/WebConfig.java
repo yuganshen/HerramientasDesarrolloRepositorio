944f6524a2a638c93d8154bb2260e45250ab0e9a
Luis Fernando <luchohuilcahusca@gmail.com>
2025-12-05 18:48:23 -0500

    controller producto funcionando correctamnte

---
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Seguridad/WebConfig.java
@@ -0,0 +1,18 @@
+package com.herramientas.desarrollo.Seguridad;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/img/**")
+        .addResourceLocations("file:C:/otros2/HerramientasDesarrolloRepositorio/uploads/img/");
+    }
+}
