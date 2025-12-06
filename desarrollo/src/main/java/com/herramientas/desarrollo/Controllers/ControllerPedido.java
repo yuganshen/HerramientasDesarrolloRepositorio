43608fce89d7b46bd721a9ab80f62fba51265aab
Luis Fernando <luchohuilcahusca@gmail.com>
2025-12-05 21:33:53 -0500

    controller gestion pedidos

--- desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllerPedido.java
+++ desarrollo/src/main/java/com/herramientas/desarrollo/Controllers/ControllerPedido.java
@@ -32,7 +32,25 @@ public class ControllerPedido {
 
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
+    
+	@GetMapping("/obtenerPedidos")
+	public ResponseEntity<List<Pedido>> obtenerPedidos() {
+	    return ResponseEntity.ok(pedidoRepositorio.findAll());
+	}
+	@PutMapping("/cambiarEstado/{id}")
+	public ResponseEntity<Pedido> cambiarEstado(@PathVariable("id") Long id) {
+	    Optional<Pedido> pedidoOpt = pedidoRepositorio.findById(id);
+	    if (pedidoOpt.isPresent()) {
+	        Pedido pedido = pedidoOpt.get();
+	        pedido.setEstado("ENTREGADO");
+	        pedidoRepositorio.save(pedido);
+	        return ResponseEntity.ok(pedido);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
+	}
 
+    
     @PostMapping
     public ResponseEntity<?> crearPedido(@RequestBody PedidoRequest request) {
         // Validaciones básicas
@@ -97,4 +115,6 @@ public class ControllerPedido {
         resp.put("total", pedidoGuardado.getTotal());
         return ResponseEntity.ok(resp);
     }
+    
+    
 }
